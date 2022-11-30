8ecd26fc2f08b041613c32cfabba054d19d59c75
ManvelManucharyan <112545060+ManvelManucharyan@users.noreply.github.com>
2022-11-30 15:41:45 +0400

    auth

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import 'module-alias/register';
 import validateEnv from '@/utils/validateEnv';
 import App from './app';
 import PostController from '@/resources/post/post.controllers';
+import UserController from '@/resources/user/user.controllers'
 
 validateEnv();
 
-const app = new App([new PostController()], Number(process.env.PORT));
+const app = new App([new PostController(), new UserController()], Number(process.env.PORT));
 
 app.listen();
